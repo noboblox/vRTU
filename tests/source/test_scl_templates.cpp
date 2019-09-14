840704e9f9969f577980e102f2a914cb3acffc90
René Büttgen <rene.buettgen1@rwth-aachen.de>
2019-09-14 16:54:17 +0200

    FIX: Remove macros for construction.

--- tests/source/test_scl_templates.cpp
+++ tests/source/test_scl_templates.cpp
@@ -29,19 +29,19 @@ BOOST_AUTO_TEST_CASE(test_enumtype)
 
 BOOST_AUTO_TEST_CASE(test_data_attribute)
 {
-	BOOST_REQUIRE_NO_THROW(SCL::BasicDataAttribute f_bda("f", SCL::BasicDataAttribute::FLOAT32)); // Basic type
-	BOOST_REQUIRE_NO_THROW(SCL::BasicDataAttribute i_bda("i", SCL::BasicDataAttribute::INT32));
+	SCL::BasicDataAttribute f_bda("f", SCL::BasicDataAttribute::FLOAT32); // Basic type
+	SCL::BasicDataAttribute i_bda("i", SCL::BasicDataAttribute::INT32);
 	
-	BOOST_REQUIRE_NO_THROW(SCL::DataAttributeType analogue_val("analogue_value1"));
+	SCL::DataAttributeType analogue_val("analogue_value1");
 	analogue_val.Insert(f_bda);
     analogue_val.Insert(i_bda);
 
-	BOOST_REQUIRE_NO_THROW(SCL::BasicDataAttribute mag("mag", analogue_val));
-	BOOST_REQUIRE_NO_THROW(SCL::BasicDataAttribute ang("ang", analogue_val));
-	BOOST_REQUIRE_NO_THROW(SCL::DataAttributeType vector("vector1"));
-	vector.Insert(mag);
-	vector.Insert(ang);
+	SCL::BasicDataAttribute mag("mag", analogue_val);
+	SCL::BasicDataAttribute ang("ang", analogue_val);
+	SCL::DataAttributeType scl_vector("vector1");
+	scl_vector.Insert(mag);
+	scl_vector.Insert(ang);
 
-	BOOST_REQUIRE_NO_THROW(SCL::DataAttribute cVal("cVal", vector, SCL::DataAttribute::MX, SCL::DataAttribute::dchg));
-	BOOST_REQUIRE_NO_THROW(SCL::DataAttribute t("t", SCL::BasicDataAttribute::Timestamp, SCL::DataAttribute::MX, SCL::DataAttribute::no_triggers));
+	SCL::DataAttribute cVal("cVal", vector, SCL::DataAttribute::MX, SCL::DataAttribute::dchg);
+	SCL::DataAttribute t("t", SCL::BasicDataAttribute::Timestamp, SCL::DataAttribute::MX, SCL::DataAttribute::no_triggers);
 }
