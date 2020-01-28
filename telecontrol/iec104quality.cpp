546a5a3f87084196a78934ca5ab0d344754bca38
René Büttgen <rene.buettgen1@rwth-aachen.de>
2020-01-28 22:40:44 +0100

    FIX: Missing formal definition of static members.
    Signed-off-by: René Büttgen <rene.buettgen1@rwth-aachen.de>

--- telecontrol/iec104quality.cpp
+++ telecontrol/iec104quality.cpp
@@ -1,5 +1,23 @@
 #include "iec104quality.hpp"
 
+constexpr uint8_t IEC104::Quality::sValueGood;
+constexpr uint8_t IEC104::Quality::sFlagOverflow;
+constexpr uint8_t IEC104::Quality::sFlagReserved;
+constexpr uint8_t IEC104::Quality::sFlagTimeout;
+constexpr uint8_t IEC104::Quality::sFlagBlocked;
+constexpr uint8_t IEC104::Quality::sFlagSubstituted;
+constexpr uint8_t IEC104::Quality::sFlagNonTopical;
+constexpr uint8_t IEC104::Quality::sFlagInvalid;
+
+constexpr const char* IEC104::Quality::sStringGood;
+constexpr const char* IEC104::Quality::sStringOverflow;
+constexpr const char* IEC104::Quality::sStringReserved;
+constexpr const char* IEC104::Quality::sStringTimeout;
+constexpr const char* IEC104::Quality::sStringBlocked;
+constexpr const char* IEC104::Quality::sStringSubstituted;
+constexpr const char* IEC104::Quality::sStringNonTopical;
+constexpr const char* IEC104::Quality::sStringInvalid;
+
 IEC104::Quality::FlagMap IEC104::Quality::msFlagNames
 {
   { sStringOverflow,    sFlagOverflow },
